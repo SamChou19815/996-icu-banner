23f81191aa1aa079807fadac52f7d34ca58958c1
Sam <sam@developersam.com>
2019-04-05 16:24:05 -0400

    Updates

---
+++ precache-manifest.35e97f28f24049d1dbb3d711da97ec23.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "4f0289e15e81fd99efd4",
+    "url": "/static/js/main.3a862a61.chunk.js"
+  },
+  {
+    "revision": "7040db331a4801a9a3d3",
+    "url": "/static/js/2.a79a0a48.chunk.js"
+  },
+  {
+    "revision": "4f0289e15e81fd99efd4",
+    "url": "/static/css/main.9b19cf32.chunk.css"
+  },
+  {
+    "revision": "fbbf806b65713ac3f2dcb2aa9093fb07",
+    "url": "/index.html"
+  }
+];
