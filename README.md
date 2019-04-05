d3173d752b55a6900f69cf006d27120d44a9b84e
Sam <sam@developersam.com>
2019-04-05 16:11:28 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,69 @@
+# 996-icu-banner
+
+[![996.icu](https://img.shields.io/badge/link-996.icu-red.svg)](https://996.icu)
+[![LICENSE](https://img.shields.io/badge/license-Anti%20996-blue.svg)](https://github.com/996icu/996.ICU/blob/master/LICENSE)
+
+A React banner component that shows your support for [996.icu](https://github.com/996icu/996.ICU).
+
+## Background
+
+[996.icu](https://github.com/996icu/996.ICU) is currently the second most starred project on GitHub.
+Some Chinese browsers [blocked the repo page](https://github.com/996icu/996.ICU/pull/24904). Let's
+add the banner to our personal website and also get ourselves blocked!
+
+(Inspired by [Lets-get-arrested project](https://github.com/hamukazu/lets-get-arrested)).
+
+## Usage
+
+You can install it by:
+
+- yarn: `yarn add @dev-sam/996-icu-banner`
+- npm: `npm i @dev-sam/996-icu-banner`
+
+```jsx
+import Banner from '@dev-sam/996-icu-banner'
+
+// anywhere inside your react app
+// let's use App as an example
+
+export default function App() {
+  return (
+    <div>
+      ... Banner does not need to be the first element
+      <Banner />
+      ... some other stuff
+    </div>
+  );
+}
+```
+
+The component supports some customization. Here are a list of all the props it accepts:
+
+```typescript
+type Languages = 'en_US' | 'zh-Hans';
+type Props = {
+  readonly bannerPosition: 'top' | 'bottom';
+  readonly lang: Languages;
+  readonly className: string | undefined;
+  readonly styles: CSSProperties | undefined;
+}
+```
+
+You can also check that directly in [`index.tsx`](./src/index.tsx).
+
+Each DOM element has a unique ID. You can directly style them using CSS or passing in `className` or
+`styles` props to style the top-level banner.
+
+## Contribution
+
+You can contribute by:
+
+- adding more translations
+- support better customization
+- etc
+
+## License
+
+Anti 996 License. License Banner displayed again to show my support.
+
+[![LICENSE](https://img.shields.io/badge/license-Anti%20996-blue.svg)](https://github.com/996icu/996.ICU/blob/master/LICENSE)
---
+++ src/index.tsx
@@ -0,0 +1,85 @@
+import React, { CSSProperties, Component } from 'react';
+
+type Languages = 'en_US' | 'zh-Hans';
+
+type I18nString = { readonly [lang in Languages]: string };
+
+const messages: I18nString = {
+  en_US: 'I support the cause of 996.icu.',
+  'zh-Hans': '我支持 996.icu。'
+};
+
+const defaultBannerStyles: CSSProperties = {
+  position: 'fixed',
+  display: 'flex',
+  alignContent: 'center',
+  justifyContent: 'center',
+  padding: '1em',
+  left: '0',
+  right: '0',
+  backgroundColor: 'white'
+};
+
+const defaultTextStyles: CSSProperties = {
+  margin: '0 1em'
+};
+
+type Props = {
+  readonly bannerPosition: 'top' | 'bottom';
+  readonly lang: Languages;
+  readonly className: string | undefined;
+  readonly styles: CSSProperties | undefined;
+};
+
+type State = { closed: boolean };
+
+export default class Banner extends Component<Props, State> {
+  static defaultProps = {
+    bannerPosition: 'top',
+    lang: 'en_US'
+  };
+
+  state = { closed: Boolean(localStorage.getItem('996-icu-banner-closed') || false) };
+
+  close = () =>
+    this.setState({ closed: true }, () => localStorage.setItem('996-icu-banner-closed', 'true'));
+
+  render() {
+    const { closed } = this.state;
+    if (closed) {
+      return null;
+    }
+    const { bannerPosition, lang, className, styles } = this.props;
+    const positionStyle: CSSProperties = {
+      ...defaultBannerStyles,
+      ...styles,
+      [bannerPosition]: '0'
+    };
+    return (
+      <div id="banner-996-icu" className={className} style={positionStyle}>
+        <span id="message-996-icu">{messages[lang]}</span>
+        <a
+          id="website-link-996-icu"
+          href="https://996.icu/"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={defaultTextStyles}
+        >
+          <img src="https://img.shields.io/badge/link-996.icu-red.svg" alt="996.icu" />
+        </a>
+        <a
+          id="github-link-996-icu"
+          href="https://github.com/996icu/996.icu"
+          target="_blank"
+          rel="noopener noreferrer"
+          style={defaultTextStyles}
+        >
+          <img src="https://img.shields.io/badge/link-996.icu%20GitHub-blue.svg" alt="996.icu" />
+        </a>
+        <a id="dismiss-button-996-icu" href="#" onClick={this.close} style={defaultTextStyles}>
+          Dismiss
+        </a>
+      </div>
+    );
+  }
+}
